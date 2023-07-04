f27b5225816ad6a44ca43a8e8ac38f01290cc4b8
LarkenB <60477169+LarkenB@users.noreply.github.com>
2023-07-04 01:22:02 -0400

    Add: Messing with Skeleton

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,6 +1,5 @@
 <script>
     import { invoke } from '@tauri-apps/api/tauri'
-    import { FileButton } from '@skeletonlabs/skeleton';
 
     let name = ''
     let greetMsg = ''
@@ -11,10 +10,13 @@
   </script>
   
   <div>
-    <span>
-      <input class="input" type="text" id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
-      <button type="button" class="btn variant-filled" on:click="{greet}">Greet</button>
-    </span>
-    <p>{greetMsg}</p>
-    <FileButton name="file"/>
+    <div class="flex flex-row justify-between w-1/4 pl-4">
+      <input type="text" class="input pl-2" id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
+      <button type="button" class="btn btn-sm variant-filled ml-2" on:click="{greet}">Greet</button>
+    </div>
+    <div class="flex justify-center m-10">
+      <div class="w-screen">
+        <h1 class="bg-gradient-to-br text-3xl from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone text-center">{greetMsg}</h1>
+      </div>
+    </div>
   </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,11 @@
     import Greet from '../lib/Greet.svelte'
     import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
-  
-<h1 class="h1">Welcome to SvelteKit</h1>
-<LightSwitch />
+
+<span class="flex justify-between items-center p-4">
+    <h1 class="bg-gradient-to-br text-5xl from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone">
+        Welcome to SvelteKit
+    </h1>
+    <LightSwitch />
+</span>
 <Greet />
