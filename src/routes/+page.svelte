70c3acce36ca4442fae84b35de5dc7fbdc60a37e
LarkenB <60477169+LarkenB@users.noreply.github.com>
2023-07-04 00:49:33 -0400

    Add: LightSwitch (light/dark toggle)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import Greet from '../lib/Greet.svelte'
+    import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
   
 <h1 class="h1">Welcome to SvelteKit</h1>
+<LightSwitch />
 <Greet />
